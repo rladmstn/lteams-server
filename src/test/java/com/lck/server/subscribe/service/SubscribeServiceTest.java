1f369c2aaff4ebf27b6a717a276d0689a18fcfd0
rladmstn <oz33177351@gmail.com>
2024-07-05 19:07:26 +0900

    test : 팀 구독 취소 관련 테스트 작성
    팀 구독 취소 성공, 이미 구독하지 않은 팀 및 권한 없음에 의한 구독 취소 실패 테스트 작성

--- src/test/java/com/lck/server/subscribe/service/SubscribeServiceTest.java
+++ src/test/java/com/lck/server/subscribe/service/SubscribeServiceTest.java
@@ -27,6 +27,7 @@ import com.lck.server.subscribe.dto.GetSubscribeResponse;
 import com.lck.server.subscribe.exception.SubscribeValidationException;
 import com.lck.server.subscribe.repository.SubscribeRepository;
 import com.lck.server.user.domain.User;
+import com.lck.server.user.exception.UserValidationException;
 import com.lck.server.user.repository.UserRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -39,16 +40,28 @@ class SubscribeServiceTest {
 	private UserRepository userRepository;
 
 	private User user;
+	private User diffUser;
+	private Subscribe subscribe;
 	@Captor
 	private ArgumentCaptor<Subscribe> subscribeCaptor;
 	@BeforeEach
 	void setUp() throws NoSuchFieldException, IllegalAccessException {
-		user = User.builder().email("email").password("password").nickname("nickname").profileImage("profileImage").subscribedTeamCount(0)
+		user = User.builder().email("email").password("password").nickname("nickname").profileImage("profileImage").subscribedTeamCount(1)
 			.nicknameChangeableDate(LocalDate.now()).role(Role.USER).build();
+		diffUser = User.builder().email("diffEmail").password("diffPassword").nickname("diffNickname").profileImage("diffProfileImage").subscribedTeamCount(1)
+			.nicknameChangeableDate(LocalDate.now()).role(Role.USER).build();
+		subscribe = Subscribe.builder().user(user).team(Team.T1).subscribedDate(LocalDateTime.now())
+			.subscriptionOrder(1).permission(true).build();
 
 		Field userField = User.class.getDeclaredField("id");
 		userField.setAccessible(true);
 		userField.set(user,1L);
+		userField.set(diffUser,2L);
+
+		Field subscribeField = Subscribe.class.getDeclaredField("id");
+		subscribeField.setAccessible(true);
+		subscribeField.set(subscribe,10L);
+
 	}
 	@Test
 	@DisplayName("팀 구독 성공")
@@ -62,8 +75,8 @@ class SubscribeServiceTest {
 		Subscribe res = subscribeCaptor.getValue();
 		assertThat(res.getTeam()).isEqualTo(Team.T1);
 		assertThat(res.getUser()).isEqualTo(user);
-		assertThat(res.getUser().getSubscribedTeamCount()).isEqualTo(1);
-		assertThat(res.getSubscriptionOrder()).isEqualTo(1);
+		assertThat(res.getUser().getSubscribedTeamCount()).isEqualTo(2);
+		assertThat(res.getSubscriptionOrder()).isEqualTo(2);
 	}
 
 	@Test
@@ -99,5 +112,42 @@ class SubscribeServiceTest {
 			assertThat(result.get(i).team()).isEqualTo(Team.T1.getName());
 			assertThat(result.get(i).subscriptionOrder()).isEqualTo(i);
 		}
+	}
+
+	@Test
+	@DisplayName("팀 구독 취소")
+	public void unsubscribeTeam(){
+		// given
+		when(userRepository.findById(anyLong())).thenReturn(Optional.ofNullable(user));
+		when(subscribeRepository.findById(anyLong())).thenReturn(Optional.ofNullable(subscribe));
+		// when
+		subscribeService.unsubscribeTeam(user,10L);
+		// then
+		verify(subscribeRepository,times(1)).delete(subscribe);
+		assertThat(user.getSubscribedTeamCount()).isEqualTo(0);
+	}
+
+	@Test
+	@DisplayName("팀 구독 취소 실패 : 이미 구독하지 않은 팀")
+	void unsubscribeTeamFailedSubscribeValidationException(){
+		// given
+		when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
+		when(subscribeRepository.findById(anyLong())).thenReturn(Optional.empty());
+		// when, then
+		assertThatThrownBy(() -> subscribeService.unsubscribeTeam(user,10L))
+			.isInstanceOf(SubscribeValidationException.class)
+			.hasFieldOrPropertyWithValue("error","이미 구독하지 않은 팀 입니다.");
+	}
+
+	@Test
+	@DisplayName("팀 구독 취소 실패 : 구독 취소 권한 없음")
+	void unsubscribeTeamFailedUserValidationException(){
+		// given
+		when(userRepository.findById(anyLong())).thenReturn(Optional.of(diffUser));
+		when(subscribeRepository.findById(anyLong())).thenReturn(Optional.ofNullable(subscribe));
+		// when, then
+		assertThatThrownBy(() -> subscribeService.unsubscribeTeam(diffUser,10L))
+			.isInstanceOf(UserValidationException.class)
+			.hasFieldOrPropertyWithValue("error","구독 취소에 대한 권한이 없습니다.");
 	}
 }
